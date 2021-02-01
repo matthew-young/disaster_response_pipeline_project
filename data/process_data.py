a9afa6d4b4061d5fb91d50a73fd35c0067830a7c
matthew-young <35228041+matthew-young@users.noreply.github.com>
2021-02-01 16:34:11 -0400

    bug fix
    - fixed error in clean_data()

--- data/process_data.py
+++ data/process_data.py
@@ -19,7 +19,7 @@ def load_data(messages_filepath, categories_filepath):
 def clean_data(df):
     
     # create a dataframe of the 36 individual category columns
-    categories = categories['categories'].str.split(pat=';',expand=True)
+    categories = df['categories'].str.split(pat=';',expand=True)
     categories.head()
     categories.shape
 
